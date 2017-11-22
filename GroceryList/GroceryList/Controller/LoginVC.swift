e830a029909da4997b6ffdacb026721497837486
Mitchell Schuster <mgschuster11@gmail.com>
2017-11-22 19:09:14 +0300

    authentication and creating users complete!

--- GroceryList/GroceryList/Controller/LoginVC.swift
+++ GroceryList/GroceryList/Controller/LoginVC.swift
@@ -23,6 +23,16 @@ class LoginVC: UIViewController {
     
     // Actions
     @IBAction func loginBtnWasPressed(_ sender: Any) {
+        if emailField.text != nil && passwordField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!, loginComplete: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                    print("Logged in successfully")
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+            })
+        }
     }
     
     @IBAction func signUpBtnWasPressed(_ sender: Any) {
