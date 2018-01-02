c6defe534eb55ef413f75c06f51cd37434c80a4b
Mitchell Schuster <mgschuster11@gmail.com>
2018-01-02 09:37:23 -0800

    Dismiss detail

--- GroceryList/GroceryList/Controller/CreateAccountVC.swift
+++ GroceryList/GroceryList/Controller/CreateAccountVC.swift
@@ -91,8 +91,9 @@ class CreateAccountVC: UIViewController {
                                         do {
                                             self.successHaptic()
                                             try Auth.auth().signOut()
-                                            let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
-                                            self.present(loginVC!, animated: true, completion: nil)
+                                            self.dismiss(animated: true, completion: nil)
+//                                            let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+//                                            self.present(loginVC!, animated: true, completion: nil)
                                         } catch {
                                             print(error)
                                         }
